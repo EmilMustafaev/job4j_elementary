025b1f98cd260c3b638e1162380cbcf1adc1bf22
EmilMustafaev <emilmustafaev891@gmail.com>
2024-02-20 10:48:52 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Emil Mustafaev");
+        System.out.println("04.01.2001");
+    }
+}
