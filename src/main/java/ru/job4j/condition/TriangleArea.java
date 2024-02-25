dd0f95fa13055a6dfd9dde9903ece44255e7adfe
EmilMustafaev <emilmustafaev891@gmail.com>
2024-02-25 09:13:24 +0300

    9. Площадь треугольника. [#175474 #482951]

--- src/main/java/ru/job4j/condition/TriangleArea.java
+++ src/main/java/ru/job4j/condition/TriangleArea.java
@@ -3,12 +3,7 @@ package ru.job4j.condition;
 public class TriangleArea {
     public static double area(double a, double b, double c) {
         double p = (a + b + c) / 2;
-        double one = p - a;
-        double two = p - b;
-        double three = p - c;
-        double four = p * one * two * three;
-        double result = Math.sqrt(four);
-        return result;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
     }
 
     public static void main(String[] args) {
