1cc6b26b70b26b1d7b49dbd44f34b04e5a258f6f
EmilMustafaev <emilmustafaev891@gmail.com>
2024-03-02 10:49:04 +0300

    4.3.3. Логическое отрицание ! [#334072]

---
+++ src/test/java/ru/job4j/condition/LogicNotTest.java
@@ -0,0 +1,135 @@
+package ru.job4j.condition;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LogicNotTest {
+    @Test
+    void whenIsEvenTrue() {
+        int num = 2;
+        boolean result = LogicNot.isEven(num);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void whenIsEvenFalse() {
+        int num = 3;
+        boolean result = LogicNot.isEven(num);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void whenIsPositiveTrue() {
+        int num = 2;
+        boolean result = LogicNot.isPositive(num);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void whenIsPositiveFalse() {
+        int num = -2;
+        boolean result = LogicNot.isPositive(num);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void whenNumIs0IsPositiveFalse() {
+        int num = 0;
+        boolean result = LogicNot.isPositive(num);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void whenNotEvenFalse() {
+        int num = 2;
+        boolean result = LogicNot.notEven(num);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void whenNotEvenTrue() {
+        int num = 3;
+        boolean result = LogicNot.notEven(num);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void whenNotPositiveTrue() {
+        int num = -2;
+        boolean result = LogicNot.notPositive(num);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void whenNotPositiveFalse() {
+        int num = 2;
+        boolean result = LogicNot.notPositive(num);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void whenNumIs0NotPositiveTrue() {
+        int num = 0;
+        boolean result = LogicNot.notPositive(num);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void whenNotEvenAndPositiveIsTrue() {
+        int num = -3;
+        boolean result = LogicNot.notEvenAndPositive(num);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void whenNotEvenFalseIsAllFalse() {
+        int num = 2;
+        boolean result = LogicNot.notEvenAndPositive(num);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void whenPositiveFalseIsAllFalse() {
+        int num = 3;
+        boolean result = LogicNot.notEvenAndPositive(num);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void whenNotEvenAndPositiveIsFalse() {
+        int num = -2;
+        boolean result = LogicNot.notEvenAndPositive(num);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void whenEvenOrNotPositiveIsTrue() {
+        int num = -2;
+        boolean result = LogicNot.evenOrNotPositive(num);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void whenEvenIsTrueThenAllIsTrue() {
+        int num = 2;
+        boolean result = LogicNot.evenOrNotPositive(num);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void whenNotPositiveIsTrueThenAllIsTrue() {
+        int num = -3;
+        boolean result = LogicNot.evenOrNotPositive(num);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void whenEvenOrNotPositiveIsFalse() {
+        int num = 5;
+        boolean result = LogicNot.evenOrNotPositive(num);
+        assertThat(result).isFalse();
+    }
+
+}
