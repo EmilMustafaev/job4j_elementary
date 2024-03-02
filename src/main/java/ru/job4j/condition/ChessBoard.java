5ce49c3c4ecb3d15209415ff2b18d0be0e9e65de
EmilMustafaev <emilmustafaev891@gmail.com>
2024-03-02 14:07:06 +0300

    4.3.6. Math.abs [#373616]

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -5,7 +5,8 @@ public class ChessBoard {
         int result = 0;
         if (isValid(x1) && isValid(x2)
                 && isValid(y1) && isValid(y2)) {
-            if (Math.abs(x2 - x1) == Math.abs(y2 - y1) && !(x2 == x1 && y2 == y1)) {
+            if (Math.abs(x2 - x1) == Math.abs(y2 - y1)
+                    && !(x2 == x1 && y2 == y1)) {
                 result = Math.abs(x2 - x1);
 
             } else {
--- src/test/java/ru/job4j/condition/ChessBoardTest.java
+++ src/test/java/ru/job4j/condition/ChessBoardTest.java
@@ -1,7 +1,6 @@
 package ru.job4j.condition;
 
 import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
