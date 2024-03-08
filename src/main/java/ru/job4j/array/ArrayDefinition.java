17bbe41a56131a76c7d528b8b932763265722147
EmilMustafaev <emilmustafaev891@gmail.com>
2024-03-08 09:16:44 +0300

    6.0.2. Размер массива [#333582]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,8 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        System.out.println("Размер массива ages: " + ages.length);
+        System.out.println("Размер массива surnames: " + surnames.length);
+        System.out.println("Размер массива prices: " + prices.length);
     }
 }
